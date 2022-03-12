6ff5d42b38d57ce036a25db035619196987fec17
Huyhomhinh0411 <101450728+Huyhomhinh0411@users.noreply.github.com>
2022-03-12 15:35:09 +0700

    Add files via upload

---
+++ baitap1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"Em ten la Nguyen Quang Huy"<<endl;
+	cout<<"MSSV:106210034"<<endl;
+	cout<<"Em hoc lop 21DTCLC1"<<endl;
+	return 0;
+	}
